c3de0b8ace0b56bf9f359d9b54b6c675996a990d
TristanMarshall <tristanmarshall@ymail.com>
2017-04-29 15:20:45 -0500

    dialog completed, validation almost complete

---
+++ scripts/validate.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+	// Set header login state to "login" on ready
+	$(".login-account")[0].innerHTML = "LOGIN";
+
+	// Close dialog
+	$("#cancelLogin").click(function() {
+		$(".validation-div")[0].innerHTML = "";
+		$(".email-input").val("");
+		$(".password-input").val("");
+		$("#dialogoverlay").fadeToggle( "slow", function() {
+		});
+	});
+
+	// Submit email / password validation
+	$("#submitLogin").click(function() {
+
+		// get password, email value
+		var password = $("#passwordcheck").val();
+		var emailaddress = $("#emailinput").val();
+
+		// filter for valid email address
+		var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+		// check for empty input fields
+		if (password.length === 0 && emailaddress.length === 0 ) {
+			$(".validation-div")[0].innerHTML = "Please enter an Email Address & Password";
+			return;
+		}
+
+		// check to see if length is greater than 6
+		if (password.length < 7 ) {
+		$(".validation-div")[0].innerHTML = "Password must be greater than 6 characters";
+		return;
+		} 
+
+		if(!filter.test(emailaddress)) {
+
+			$(".validation-div")[0].innerHTML = "Please enter a valid email address";
+			return;
+		} 
+
+		// reset all values / close dialog
+		$(".login-account")[0].innerHTML = "ACCOUNT";
+		$(".validation-div")[0].innerHTML = "";
+		$(".email-input").val("");
+		$(".password-input").val("");
+		$("#dialogoverlay").fadeToggle( "slow", function() {
+		});
+	});
+
+
+});
